353831e72e1f76b8055b743b9b0c190ef5cf4437
talhairtisam <68581950+talhairtisam@users.noreply.github.com>
2020-12-21 01:40:14 +0500

    Update app.py

--- app.py
+++ app.py
@@ -38,7 +38,7 @@ def favicon():
     return send_from_directory(os.path.join(app.root_path, 'static'),
                                'favicon.ico', mimetype='image/vnd.microsoft.icon')
 
-@app.route('/')
+@app.route('/',methods=['GET'])
 def index():
     return render_template('index.html', errorM = 'fade')
 
